c5168f8e6bf8dc12fb0fde6ea5baee7f4f7edb6e
Tav Hafner <23143092+haftav@users.noreply.github.com>
2021-12-18 23:23:16 -0700

    Structure updates

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,8 @@
 #!/usr/bin/env node
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
-import prompts from 'prompts';
+import prompts, { PromptObject } from 'prompts';
 import invariant from 'tiny-invariant';
-
-interface Command {
-  title: string;
-  value: string;
-}
-
-// hardcoding commands for now - these will eventually be defined in config file
-const commands: Command[] = [{ title: 'Component', value: 'component' }];
-
-async function init() {
-  yargs(hideBin(process.argv))
-    .scriptName('filestamp')
-    .usage('$0 [command] [args]')
-    .command(
-      '$0 [command]',
-      'Specifies which scaffold command to run',
-      (yargs) => {
-        yargs.positional('command', {
-          type: 'string',
-          describe: 'the command to run',
-        });
-      },
-      function (argv) {
-        filestamp(argv);
-      }
-    )
-    .help()
-    .parse();
-}
 
 interface InitialArgs {
   [argName: string]: unknown;
@@ -41,6 +12,92 @@ interface InitialArgs {
 
 type Args = Omit<InitialArgs, '_' | '$0'>;
 type CommandLineVariables = Omit<Args, 'command'>;
+
+interface Command {
+  title: string;
+  value: string;
+}
+
+// hardcoded right now
+const userPrompts: PromptObject[] = [
+  {
+    type: 'text',
+    name: 'name',
+    message: 'What is the name of your component?',
+  },
+  {
+    type: 'number',
+    name: 'age',
+    message: 'How old are you?',
+  },
+  {
+    type: 'text',
+    name: 'about',
+    message: 'Tell something about yourself',
+    initial: 'Why should I?',
+  },
+];
+
+function init() {
+  return yargs
+    .scriptName('filestamp')
+    .usage('$0 [command] [args]')
+    .command('$0 [command]', 'Specifies which scaffold command to run', (yargs) => {
+      yargs.positional('command', {
+        type: 'string',
+        describe: 'the command to run',
+      });
+    })
+    .help().argv;
+}
+
+function create(argv: InitialArgs) {
+  const argsObject = stripIgnoredArgs(argv);
+
+  async function main() {
+    const command = await getCommand(argsObject);
+
+    invariant(typeof command === 'string', 'Command must be a string.');
+
+    // TODO: change this to use function exported from user config
+    handleCommand(command);
+
+    return argv;
+  }
+
+  function createFilestamp() {
+    return function filestamp(currentPath: string, ...args: Array<unknown>) {
+      // TODO: add stuff
+    };
+  }
+
+  function createPropCollector() {
+    return function collectProps(fn: () => PromptObject[]) {
+      // read all variables from CLI
+      const cliVariables = getCommandLineVariables(argsObject);
+
+      // Override any prompts with variables that were supplied through CLI
+      prompts.override(cliVariables);
+
+      // Get list of user prompst
+      const userPrompts = fn();
+
+      // Execute prompts -> will skip if all prompts overriden through CLI
+      const answers = prompts(userPrompts);
+
+      return answers;
+    };
+  }
+
+  const collectProps = createPropCollector();
+  const filestamp = createFilestamp();
+
+  return {
+    main,
+    filestamp,
+    collectProps,
+  };
+}
 
 function stripIgnoredArgs(argv: InitialArgs): Args {
   const output = { ...argv } as Args;
@@ -56,29 +113,8 @@ function stripIgnoredArgs(argv: InitialArgs): Args {
   return output;
 }
 
-// strip out any 'command' option supplied by user so we're left with just variable arguments
-function getCommandLineVariables(argsObject: Args): CommandLineVariables {
-  const output = { ...argsObject } as CommandLineVariables;
-
-  if ('command' in output) {
-    delete output.command;
-  }
-
-  return output;
-}
-
-async function filestamp(argv: InitialArgs) {
-  const argsObject = stripIgnoredArgs(argv);
-  const command = await getCommand(argsObject);
-
-  invariant(typeof command === 'string', 'Command must be a string.');
-
-  console.log('argv', argv);
-  console.log('commandLineArgs', argsObject);
-  console.log(command);
-
-  const argsVariables = getCommandLineVariables(argsObject);
-}
+// hardcoding commands for now - these will eventually be defined in config file
+const commands: Command[] = [{ title: 'Component', value: 'component' }];
 
 async function getCommand(argsObject: Args) {
   if ('command' in argsObject) {
@@ -95,4 +131,46 @@ async function getCommand(argsObject: Args) {
   return command;
 }
 
-init().catch((err) => console.error(err));
+type File = () => void;
+type Folder = () => void;
+
+// strip out any 'command' option supplied by user so we're left with just variable arguments
+function getCommandLineVariables(argsObject: Args): CommandLineVariables {
+  const output = { ...argsObject } as CommandLineVariables;
+
+  if ('command' in output) {
+    delete output.command;
+  }
+
+  return output;
+}
+
+/*
+===========================================================================
+*/
+
+// EXAMPLE USER CODE
+
+// import filestamp from 'filestamp'
+
+async function handleCommand(command: string) {
+  if (command === 'component') {
+    const props = await collectProps(() => userPrompts);
+    filestamp('./components', props);
+  }
+  if (command === 'hook') {
+    filestamp('./hooks');
+  }
+}
+
+/*
+===========================================================================
+*/
+
+const argv = init();
+
+const { main, collectProps, filestamp } = create(argv as InitialArgs);
+
+main();
+
+export { collectProps, filestamp };
